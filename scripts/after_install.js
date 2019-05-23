97f74f3a69cd40417ed1d97ead19972a7720a6e0
Kano Developer <opensource@kanoapps.com>
2019-05-23 15:21:30 -0700

    added content src variable

--- scripts/after_install.js
+++ scripts/after_install.js
@@ -22,34 +22,24 @@
 // This script modifies the project root's config.xml
 // The <content> tag "src" attribute is modified to point to http://localhost:0
 
-var content_src_value = "http://localhost:0";
 var fs = require('fs');
 var path = require('path');
-var old_content_src_value;
+var et = require('elementtree');
 
 module.exports = function(context) {
     var config_xml = path.join(context.opts.projectRoot, 'config.xml');
-    var et = context.requireCordovaModule('elementtree');
-
     var data = fs.readFileSync(config_xml).toString();
     var etree = et.parse(data);
+    var content_src = 'html';
+
+    var value = data.match(new RegExp('name="CONTENT_SRC" value="(.*?)"', "i"));
+    if(value && value[1]) {
+        content_src = value[1];
+    }
 
     var content_tags = etree.findall('./content[@src]');
     if (content_tags.length > 0) {
-        old_content_src_value = content_tags[0].get('src');
-        var backup_json = path.join(context.opts.plugin.dir, 'backup.json');
-        var backup_value = { content_src : old_content_src_value };
-        fs.writeFileSync(backup_json, JSON.stringify(backup_value));
-
-        content_tags[0].set('src', content_src_value);
-    }
-
-    var altcontentsrcTag = etree.findall("./preference[@name='AlternateContentSrc']");
-    if (altcontentsrcTag.length > 0) {
-        altcontentsrcTag[0].set('value', old_content_src_value);
-    } else {
-      var pref = et.Element('preference', { name: 'AlternateContentSrc', value: old_content_src_value });
-      etree.getroot().append(pref);
+        content_tags[0].set('src', content_src);
     }
 
     data = etree.write({'indent': 4});
